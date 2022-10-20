2a0e1b54d2c95586706d63f7c655d1cf3d80424c
maaguilar <aguilardmarkanthony@gmail.com>
2022-10-21 00:43:27 +0800

    update 1baseline

--- TradingBot.py
+++ TradingBot.py
@@ -1,15 +1,42 @@
-import pandas_ta as ta
-import numpy as np
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from datetime import datetime
-from backtesting import Strategy
-from backtesting import Backtest
-import pandas as pd
+from binance.client import Client
+api_key = 'KWVUpuWlJyVeL58wn7dHmKDx2OjJ7uAqq6eo5dZRDirq0XkgFCGe8dX6w7ryZa0m'
+api_secret = 'TqGtGj8QuNRPhJb53mPSo3QklF02gZ10XSsXJ5QzXfyggkocsZkeCOlRttcmebUQ'
+client = Client(api_key, api_secret)
 
+columns = [
+    'open_time', 'open', 'high', 'low', 'close', 'volume',
+    'close_time', 'quote_asset_volume', 'number_of_trades',
+    'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume',
+    'ignore'
+]
+klines = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_5MIN, "1 day ago UTC")
+data = pd.DataFrame(klines, columns = ['open_time','open', 'high', 'low', 'close', 'volume','close_time', 'qav','num_trades','taker_base_vol','taker_quote_vol', 'ignore'])
+
+if data.empty:
+    continue
+    
+if data2.empty:
+    continue 
+
+if data3.empty:
+    continue 
+
+if data4.empty:
+    continue
+
+
+import pandas as pd
 df=pd.read_csv("EURUSD_Candlestick_5_M_ASK_30.09.2019-30.09.2022.csv")
 df
 
+#df["Gmt time"]=df["Gmt time"].str.replace(".000","")
+df['Gmt time']=df["Gmt time"].str.replace(r'.000','', regex=True)
+df['Gmt time']=pd.to_datetime(df['Gmt time'],format='%d.%m.%Y %H:%M:%S')
+df.set_index("Gmt time", inplace=True)
+df=df[df.High!=df.Low]
+len(df)
+
+import pandas_ta as ta
 df["VWAP"]=ta.vwap(df.High, df.Low, df.Close, df.Volume)
 df['RSI']=ta.rsi(df.Close, length=16)
 my_bbands = ta.bbands(df.Close, length=14, std=2.0)
@@ -51,6 +78,9 @@ for row in range(backcandles, len(df)): #careful backcandles used previous cell
     TotSignal[row] = TotalSignal(row)
 df['TotalSignal'] = TotSignal
 
+df[df.TotalSignal!=0].count()
+
+import numpy as np
 def pointposbreak(x):
     if x['TotalSignal']==1:
         return x['High']+1e-4
@@ -61,6 +91,9 @@ def pointposbreak(x):
 
 df['pointposbreak'] = df.apply(lambda row: pointposbreak(row), axis=1)
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from datetime import datetime
 st=10400
 dfpl = df[st:st+350]
 dfpl.reset_index(inplace=True)
@@ -83,43 +116,3 @@ fig.add_scatter(x=dfpl.index, y=dfpl['pointposbreak'], mode="markers",
                 marker=dict(size=10, color="MediumPurple"),
                 name="Signal")
 fig.show()
-
-dfpl = df[:75000].copy()
-import pandas_ta as ta
-dfpl['ATR']=ta.atr(dfpl.High, dfpl.Low, dfpl.Close, length=7)
-#help(ta.atr)
-def SIGNAL():
-    return dfpl.TotalSignal
-
-
-class MyStrat(Strategy):
-    initsize = 0.99
-    mysize = initsize
-    def init(self):
-        super().init()
-        self.signal1 = self.I(SIGNAL)
-
-    def next(self):
-        super().next()
-        slatr = 1.2*self.data.ATR[-1]
-        TPSLRatio = 1.5
-
-        if len(self.trades)>0:
-            if self.trades[-1].is_long and self.data.RSI[-1]>=90:
-                self.trades[-1].close()
-            elif self.trades[-1].is_short and self.data.RSI[-1]<=10:
-                self.trades[-1].close()
-        
-        if self.signal1==2 and len(self.trades)==0:
-            sl1 = self.data.Close[-1] - slatr
-            tp1 = self.data.Close[-1] + slatr*TPSLRatio
-            self.buy(sl=sl1, tp=tp1, size=self.mysize)
-        
-        elif self.signal1==1 and len(self.trades)==0:         
-            sl1 = self.data.Close[-1] + slatr
-            tp1 = self.data.Close[-1] - slatr*TPSLRatio
-            self.sell(sl=sl1, tp=tp1, size=self.mysize)
-
-bt = Backtest(dfpl, MyStrat, cash=100, margin=1/10, commission=0.00)
-stat = bt.run()
-stat

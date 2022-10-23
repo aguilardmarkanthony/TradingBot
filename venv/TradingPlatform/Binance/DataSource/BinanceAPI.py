b295423042d32a863ef14ec69807ade6922d3da8
maaguilar <aguilardmarkanthony@gmail.com>
2022-10-24 07:01:05 +0800

    binance api

--- venv/TradingPlatform/Binance/DataSource/BinanceAPI.py
+++ venv/TradingPlatform/Binance/DataSource/BinanceAPI.py
@@ -2,11 +2,10 @@ from TradingPlatform.Binance.DataSource import BinanceSecretKey as apk
 from TradingPlatform.Binance.DataSource import BinanceAPI as binance
 from TradingPlatform.Binance.DataSource import BinanceClient as client
 
-
 def exchange_info():
   exchange_info = client.get_exchange_info()
   return exchange_info
 
-def get_historical_klines(symbol, interval, start_str=None, end_str=None):
-  historical_klines = client.get_historical_klines(symbol, interval, start_str, end_str)
+def historical_klines(symbol, interval, start_str=None, end_str=None):
+  historical_klines = client.get_historical_klines(symbol=symbol, interval=interval, start_str=start_str, end_str=end_str)
   return historical_klines
--- venv/mark_call.py
+++ venv/mark_call.py
@@ -1,7 +1,12 @@
 from TradingPlatform.Binance.DataSource import BinanceAPI as binance
 from TradingPlatform.Binance.DataSource import BinanceSecretKey as apk
+import datetime as dt
 
-exchaxnge_info = binance.exchange_info()
-print(exchaxnge_info)
+#exchange_info = binance.exchange_info()
+start = str(int(dt.datetime(2021,5,1).timestamp()*1000))
+end = str(int(dt.datetime(2021,8,1).timestamp()*1000))
+historical_klines = binance.historical_klines(symbol='BTCUSDT', interval='1hr',start_str=start, end_str=end)
+
+print(historical_klines)
 
 
